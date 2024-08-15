23090dd5f46ba5c5e1c751b3227f4908618b1635
atom <145321614+vishnu1002@users.noreply.github.com>
2024-08-15 13:56:35 +0530

    Update ccache.bat
    Many functionalities have been included

--- ccache.bat
+++ ccache.bat
@@ -1,62 +1,83 @@
 @echo off
 
-Rem Clear Cache by Vishnu
+:: Check for administrator privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo [ Requesting Administrator Access...]
+    powershell start -verb runas '%0'
+    exit /b
+)
+
+:: Clear Cache by Vishnu
 
 title Clear Cache
 cls
 
 echo.
-echo ===========================================================================
-echo.
-echo [ CLEAR ALL CATCH ]
+echo [ CLEAR ALL CACHE ]
 echo.
 
-echo Temp
+echo ===========================================================================
+echo.
+echo [ Cleaning Windows Temp ]
+echo.
 del /s /f /q C:\Windows\Temp\*.*
 echo.
-echo ##########
-echo.
 
-echo Prefetch
+echo ===========================================================================
+echo.
+echo [ Cleaning Windows Prefetch ]
+echo.
 del /s /f /q C:\Windows\Prefetch\*.*
 echo.
-echo ##########
-echo.
 
-echo TEMP
-del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
-
-echo.
 echo ===========================================================================
 echo.
-echo [ SOFTWARE DISTRIBUTION ]
+echo [ Cleaning User Temp (% TEMP %) ]
+echo.
+del /s /f /q %TEMP%\*.*
 echo.
 
+echo ===========================================================================
+echo.
+echo [ Cleaning LocalAppData Temp ]
+echo.
+del /s /f /q %LOCALAPPDATA%\Temp\*.*
+echo.
+
+echo ===========================================================================
+echo.
+echo [ CLEARING SOFTWARE DISTRIBUTION CACHE ]
+echo.
 rmdir /s /q C:\Windows\SoftwareDistribution\Download\
-
 echo.
+
 echo ===========================================================================
 echo.
-echo [ NVIDIA CACHE ]
+echo [ CLEARING NVIDIA CACHE ]
+echo.
+rmdir /s /q C:\ProgramData\NVIDIA\
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\NVIDIA\
 echo.
 
-del /s /f /q C:\ProgramData\NVIDIA\*.*
-rmdir /s /q  C:\ProgramData\NVIDIA Corporation\NV_Cache\
-rmdir /s /q  C:\Users\%USERNAME%\AppData\Local\NVIDIA\GLCache\
-rmdir /s /q  C:\Users\%USERNAME%\AppData\Local\NVIDIA\DXCache\
-rmdir /s /q  C:\Users\%USERNAME%\AppData\Local\NVIDIA Corporation\NV_Cache\
-
-echo.
 echo ===========================================================================
 echo.
-echo [ DNS FLUSH ]
+echo [ CLEARING AMD CACHE ]
+echo.
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\AMD\DxCache\
+rmdir /s /q C:\Users\%USERNAME%\AppData\Local\AMD\VkCache\
+echo.
 
+echo ===========================================================================
+echo.
+echo [ DNS FLUSH AND WINSOCK RESET ]
 ipconfig /flushdns
-
+netsh winsock reset 
 echo.
+
 echo ===========================================================================
 echo.
-echo All catch files cleared.
+echo All cache files cleared.
 echo.
 
 PAUSE
